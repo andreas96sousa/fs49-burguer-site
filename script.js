6facdbbdc587efef31616a71c77f664296164e20
Andreas Sousa <devandreas96@gmail.com>
2025-07-19 22:29:22 -0300

    site finalizado

---
+++ script.js
@@ -0,0 +1,6 @@
+let logo = document.querySelector("#logo")
+        let body = document.querySelector("#body")
+        function girarSite(){
+            body.style.transition = "3s"
+            body.style.transform = "rotateY(1440deg)"
+        }
